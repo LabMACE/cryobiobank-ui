ac736df671011bca9e9ae7cfac3f1d22e3b12506
Evan Thomas <evan.thomas@epfl.ch>
2025-01-13 18:02:02 +0100

    Add array to sites

--- src/sites/Show.jsx
+++ src/sites/Show.jsx
@@ -5,8 +5,10 @@ import {
     BooleanField,
     DateField,
     Labeled,
+    ArrayField,
+    Datagrid,
 } from 'react-admin';
-import { Grid} from '@mui/material';
+import { Grid } from '@mui/material';
 
 
 const ShowComponent = () => {
@@ -26,45 +28,19 @@ const ShowComponent = () => {
                                     <TextField source="name" />
                                 </Labeled>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Labeled label="Comment">
-                                    <TextField source="comment" />
-                                </Labeled>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Labeled label="Processing Has Started">
-                                    <BooleanField source="processing_has_started" />
-                                </Labeled>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Labeled label="Processing Success">
-                                    <BooleanField source="processing_success" />
-                                </Labeled>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Labeled label="Created on">
-                                    <DateField
-                                        source="created_on"
-                                        sortable={false}
-                                        showTime={true}
-                                        transform={value => new Date(value + 'Z')}  // Fix UTC time
-                                    />
-                                </Labeled>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Labeled label="Last updated">
-                                    <DateField
-                                        source="last_updated"
-                                        sortable={false}
-                                        showTime={true}
-                                        transform={value => new Date(value + 'Z')}  // Fix UTC time
 
-                                    />
-                                </Labeled>
-                            </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
+                <ArrayField source="replicates">
+                    <Datagrid bulkActionButtons={false}>
+                        <DateField source="sampling_date" />
+                        <TextField source="id" />
+                        <TextField source="sample_type" />
+                        <TextField source="sample_depth_cm" />
+                        <TextField source="snow_depth_cm" />
+                    </Datagrid>
+                </ArrayField>
             </SimpleShowLayout>
         </Show >
     )
