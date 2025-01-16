5a7c318041fb2cc3351d7b0e6d805bc578132ce7
Evan Thomas <evan.thomas@epfl.ch>
2025-01-16 15:18:39 +0100

    Update edit page for sites

--- src/sites/Edit.jsx
+++ src/sites/Edit.jsx
@@ -1,6 +1,10 @@
 import {
+    NumberInput,
     SimpleForm,
     TextInput,
+    minValue,
+    maxValue,
+    required,
 } from 'react-admin';
 import CustomEdit from '../custom/Edit';
 
@@ -10,7 +14,9 @@ const EditComponent = () => {
             <SimpleForm>
                 <TextInput source="id" disabled />
                 <TextInput source="name" />
-                <TextInput source="comment" />
+                <NumberInput source="y" label="Latitude (°)" validate={[required(), minValue(-90), maxValue(90)]} />
+                <NumberInput source="x" label="Longitude (°)" validate={[required(), minValue(-180), maxValue(180)]} />
+                <NumberInput source="z" label="Elevation (m)" validate={[required()]} />
             </SimpleForm>
         </CustomEdit>
     )
