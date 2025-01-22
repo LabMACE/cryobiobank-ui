a290a2620f3d8800ad1cd2155261cf4fe6d0687c
Evan Thomas <evan@evanjt.com>
2025-01-22 16:53:43 +0100

    Add coordinate entry field for Site Edit page

---
+++ src/maps/CoordinateEntry.tsx
@@ -0,0 +1,165 @@
+import {
+    required,
+    NumberInput,
+    Button,
+} from 'react-admin';
+import { MapContainer, Marker } from 'react-leaflet';
+import { useFormContext } from 'react-hook-form';
+import { useState, useEffect } from 'react';
+import { Grid, Typography } from '@mui/material';
+import proj4 from 'proj4';
+import L from 'leaflet';
+import { BaseLayers } from './Layers';
+
+// Fix leaflet's default icon issue with webpack
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+// Define the Swiss coordinate system EPSG 2056
+proj4.defs("EPSG:2056", "+proj=somerc +lat_0=46.9524055555556 +lon_0=7.43958333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs +type=crs");
+
+const ElevationInput = () => {
+    const formContext = useFormContext();
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [successResponse, setSuccessResponse] = useState(false);
+
+    const updateElevation = () => {
+        const x = formContext.getValues('coord_x');
+        const y = formContext.getValues('coord_y');
+        const url = `https://api3.geo.admin.ch/rest/services/height?easting=${x}&northing=${y}&sr=2056&format=json&geometryFormat=geojson`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.success === false) {
+                    setErrorMessage(`Error fetching elevation: ${data.error.message}`);
+                } else {
+                    setErrorMessage(null);
+                    setSuccessResponse(true);
+                    formContext.setValue('elevation_metres', data.height);
+                }
+            });
+    };
+
+    return (
+        <>
+            <Button
+                label="Get elevation from SwissTopo"
+                variant="outlined"
+                color={errorMessage ? 'error' : successResponse ? 'success' : 'primary'}
+                onClick={updateElevation}
+            />
+            <Typography variant="caption" color={'error'}>
+                {errorMessage ? errorMessage : null}
+            </Typography>
+            <NumberInput source="elevation_metres" label="Elevation (m)" />
+        </>
+    );
+};
+
+export const CoordinateInput = () => {
+    const { setValue, watch } = useFormContext();
+
+    const coord_x = watch('coord_x');
+    const coord_y = watch('coord_y');
+    const latitude_4326 = watch('latitude_4326');
+    const longitude_4326 = watch('longitude_4326');
+
+    const defaultCoordinates = [46.224413762594594, 7.359968915183943];
+    const [position, setPosition] = useState(latitude_4326 && longitude_4326 ? [latitude_4326, longitude_4326] : defaultCoordinates);
+
+    const isValidCoordinate = (value) => {
+        return typeof value === 'number' && isFinite(value);
+    };
+
+    // Handle XY to LatLon conversion
+    const updateLatLonFromXY = (x, y) => {
+        if (isValidCoordinate(x) && isValidCoordinate(y)) {
+            const [lng, lat] = proj4('EPSG:2056', 'EPSG:4326', [x, y]);
+            setValue('latitude_4326', lat, { shouldValidate: true });
+            setValue('longitude_4326', lng, { shouldValidate: true });
+            setPosition([lat, lng]);
+        }
+    };
+
+    // Handle LatLon to XY conversion
+    const updateXYFromLatLon = (lat, lng) => {
+        if (isValidCoordinate(lat) && isValidCoordinate(lng)) {
+            const [x, y] = proj4('EPSG:4326', 'EPSG:2056', [lng, lat]);
+            setValue('coord_x', Math.round(x), { shouldValidate: true });
+            setValue('coord_y', Math.round(y), { shouldValidate: true });
+            setPosition([lat, lng]);
+        }
+    };
+
+    // Update Lat/Lon when coord_x/coord_y change
+    useEffect(() => {
+        if (isValidCoordinate(coord_x) && isValidCoordinate(coord_y)) {
+            updateLatLonFromXY(coord_x, coord_y);
+        }
+    }, [coord_x, coord_y]);
+
+    // Update coord_x/coord_y when latitude_4326/longitude_4326 change
+    useEffect(() => {
+        if (isValidCoordinate(latitude_4326) && isValidCoordinate(longitude_4326)) {
+            updateXYFromLatLon(latitude_4326, longitude_4326);
+        }
+    }, [latitude_4326, longitude_4326]);
+
+    const handleSetCoords = (lat, lng) => {
+        updateXYFromLatLon(lat, lng); // Update XY and LatLon when dragging on map
+    };
+
+    return (
+        <>
+            <Grid container spacing={2} alignItems='center'>
+                {/* Left side: X/Y and Latitude/Longitude in a 2x2 grid */}
+                <Grid item xs={5}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <NumberInput source="coord_x" label="X Coordinate (m: SRID 2056)" validate={[required()]} disabled/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <NumberInput source="coord_y" label="Y Coordinate (m: SRID 2056)" validate={[required()]} disabled/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <NumberInput source="latitude_4326" label="Latitude (°)" />
+                        </Grid>
+                        <Grid item xs={6}>
+                            <NumberInput source="longitude_4326" label="Longitude (°)" />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <ElevationInput />
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                {/* Right side: Map taking up the full column */}
+                <Grid item xs={7}>
+                    <MapContainer
+                        center={position}
+                        zoom={13}
+                        style={{ height: '400px', width: '100%' }}
+                    >
+                        <BaseLayers />
+                        <Marker
+                            position={position}
+                            draggable={true}
+                            eventHandlers={{
+                                dragend(e) {
+                                    const { lat, lng } = e.target.getLatLng();
+                                    handleSetCoords(lat, lng);
+                                },
+                            }}
+                        />
+                    </MapContainer>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default CoordinateInput;
--- src/sites/Edit.jsx
+++ src/sites/Edit.jsx
@@ -2,21 +2,28 @@ import {
     NumberInput,
     SimpleForm,
     TextInput,
-    minValue,
-    maxValue,
-    required,
+    Toolbar,
+    SaveButton,
 } from 'react-admin';
 import CustomEdit from '../custom/Edit';
+import CoordinateInput from '../maps/CoordinateEntry';
+
+const MyToolbar = () => (
+    <Toolbar>
+        <SaveButton alwaysEnable />
+    </Toolbar>
+);
 
 const EditComponent = () => {
     return (
         <CustomEdit redirect="show" mutationMode="pessimistic">
-            <SimpleForm>
+            <SimpleForm toolbar={<MyToolbar />}>
                 <TextInput source="id" disabled />
                 <TextInput source="name" />
-                <NumberInput source="latitude_4326" label="Latitude (°)" validate={[required(), minValue(-90), maxValue(90)]} />
-                <NumberInput source="longitude_4326" label="Longitude (°)" validate={[required(), minValue(-180), maxValue(180)]} />
-                <NumberInput source="elevation_metres" label="Elevation (m)" validate={[required()]} />
+                {/* <NumberInput source="latitude_4326" label="Latitude (°)" validate={[required(), minValue(-90), maxValue(90)]} /> */}
+                {/* <NumberInput source="longitude_4326" label="Longitude (°)" validate={[required(), minValue(-180), maxValue(180)]} /> */}
+                {/* <NumberInput source="elevation_metres" label="Elevation (m)" validate={[required()]} /> */}
+                <CoordinateInput />
             </SimpleForm>
         </CustomEdit>
     )
