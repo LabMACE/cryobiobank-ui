5cd4452e98dddbf0396e9c5149ee94e85f74b650
Evan Thomas <evan@evanjt.com>
2025-02-06 11:05:40 +0100

    Move admin to /admin route, split into subfolder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import AdminApp from './admin/App'
 import './index.css'
+import { RouterProvider, Routes, Route, createBrowserRouter } from 'react-router-dom';
+// import { StoreFront } from './StoreFront';
+// import { StoreAdmin } from './StoreAdmin';
+
+export const App = () => {
+    const router = createBrowserRouter(
+        [
+            {
+                path: "*",
+                element: (
+                    <Routes>
+                        {/* <Route path="/" element={<StoreFront />} /> */}
+                        <Route path="/admin/*" element={<AdminApp />} />
+                    </Routes>
+                ),
+            },
+        ],
+    );
+    return <RouterProvider router={router} />;
+};
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/* <AdminApp /> */}
   </StrictMode>
 )
